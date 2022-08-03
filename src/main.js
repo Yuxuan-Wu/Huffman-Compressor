6caaa6fa912c3caf69d39c6cbd7ca913beeac7f8
Yuxuan-Wu <charleslele2002@gmail.com>
2022-08-02 22:30:29 -0700

    Prototype implemented

--- src/main.js
+++ src/main.js
@@ -9,52 +9,100 @@ const { dialog, Menu } = require("@electron/remote");
 //HTML elements
 const compressBtn = document.querySelector("#compressBtn");
 const decompressBtn = document.querySelector("#decompressBtn");
+const frequencyList = [];
 
 /**
  * Bind Event Listeners
  * ====================
  */
 
-compressBtn.onclick = selectFile;
+compressBtn.onclick = () => {
+    selectFile(compressFile);
+};
 
 decompressBtn.onclick = () => {
-
+    selectFile(decompressFile);
 };
 
 /**
  * File IO Operations
  */
-function selectFile() {
+function selectFile(action) {
     dialog.showOpenDialog({
         properties: ['openFile']
     }).then(function (response) {
         if (!response.canceled) {
             //handle file
             let filePath = response.filePaths[0];
-            let directoryPath = filePath.slice(0, filePath.lastIndexOf("\\"));
-            readFile(filePath);
-            //storeFile("compressed@" + Date.now(), directoryPath);
+            action(filePath);
         } else {
             console.log("no file selected");
         }
     });
 }
 
-function compressFile() {
+function decompressFile(path) {
+    let binaryUint8 = fileIO.readFileSync(path);
 
+    let buffer = Buffer.from(binaryUint8);
+    
+    //convert the unint8 array to a unint16 array
+    let binaryUint16 = new Uint16Array(buffer.buffer, buffer.byteOffset, buffer.byteLength / Uint16Array.BYTES_PER_ELEMENT);
+
+    console.log(binaryUint16)
 }
 
-function readFile(path) {
-    fileIO.readFile(path, "utf-8", function(err, data) {
+decompressFile("C:\\VSCode_Workspace\\Summer_2022_Project\\Text_Samples\\test");
+
+function compressFile(path) {
+    fileIO.readFile(path, "utf-8", function (err, data) {
         if (err) throw err;
 
-        console.log(data);
+        //create a frequency list from file data
+        createFreqList(data);
+
+        //generate the header for compressed file
+        let encodedContent = generateHeader(frequencyList);
+
+        //store the compressed file
+        let directoryPath = path.slice(0, path.lastIndexOf("\\"));
+        storeFile("compressed@" + Date.now(), directoryPath, encodedContent);
     });
 }
 
-function storeFile(fname, dir) {
-    fileIO.writeFile(dir + "\\" + fname + '.txt', 'Hello content!', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
+function storeFile(fname, dir, content) {
+    let buffer = Buffer.from(content.buffer);
+
+    fileIO.createWriteStream(dir + "\\" + fname).write(buffer);
 }
+
+/**
+ * Auxiliary Functions
+ */
+
+function createFreqList(data) {
+    for (var i = 0; i < data.length; i++) {
+        //populate the frequency list with characters
+        frequencyList[data.charAt(i)] = (frequencyList[data.charAt(i)] || 0) + 1;
+    }
+}
+
+function generateHeader(freqList) {
+    let header = [];
+
+    //length of the header is the first character in the encoded content
+    header.push(Object.keys(freqList).length);
+
+    //second part of the header: symbols and their frequencies
+    for (var symbol of Object.keys(freqList)) {
+        header.push(symbol.charCodeAt(0));
+        header.push(freqList[symbol]);
+    }
+
+    console.log(new Uint16Array(header))
+
+    return new Uint16Array(header);
+}
+
+//TODO: convert from char to int
+//String.charCodeAt(0);
